0a4d634361503b3e4d43239d34a1c82fa48ff7ae
r <r>
2023-09-19 21:14:13 -0600

    Creating setup.sh

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+## Author  : Enrique Leon
+## Github  : @EnriLion
+
+## termux-newsboat installer script
+
+## ANSI Colors (FG & BG)
+
+## Directories
+
+## Banner
+banner () {
+    clear
+    echo "
+    termux-newsboat
+    "
+    echo "(*) Made by: EnriLion"
+}
+
+## Script Termination
+
+## Reset terminal colors
+
+## Prerequisite
+
+## Install termux-style
+
+	# Verify files 
+
+## Main
+main () {
+	banner
+}
+
+main
